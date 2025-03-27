5743e31582528848986f22b15c4a38b1bf1c6a12
Zhu Zhanyan <program.nom@gmail.com>
2025-03-27 13:19:55 +0800

    feat(lab2): add solution to for exercise_2b

--- lab2/exercise_2b.pl
+++ lab2/exercise_2b.pl
@@ -1,16 +1,10 @@
 %
 % SC30000
 % Lab Assignment 2
-% Exercise 2a): The Royal Family
+% Exercise 2b): The Royal Family
 %
 
 % knowledge base
-% define gender facts
-female(elizabeth).
-female(ann).
-male(charles).
-male(andrew).
-male(edward).
 
 % define offspring facts
 offspring(elizabeth,charles).
@@ -18,8 +12,6 @@ offspring(elizabeth,ann).
 offspring(elizabeth,andrew).
 offspring(elizabeth,edward).
 
-
 % inference rules
 % sucession rule: male first, female second
-successor(X, Y) :- male(Y), offspring(X, Y).
-successor(X, Y) :- female(Y), offspring(X, Y).
+successor(X, Y) :- offspring(X, Y).
